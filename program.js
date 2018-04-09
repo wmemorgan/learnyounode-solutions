13a797469f2286b737efbba616f0575bcd76f9bf
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-09 05:45:48 +0000

    FILTERED LS (Exercise 5 of 13) [ES6 Version] Changes to be committed:   modified:   program.js

--- program.js
+++ program.js
@@ -1,16 +1,16 @@
 //Exercise 5: Filtered LS
-
-var fs = require('fs'),
-    path = require('path'),
-    filePath = process.argv[2],
+//ES6 format
+const fs = require('fs'),
+    path = require('path');
+    
+let filePath = process.argv[2],
     fileExt = process.argv[3];
 
-fs.readdir(filePath, function(err, items) {
-    for (var i=0; i<items.length; i++) {
-        var ext = path.extname(items[i]);
-        if (path.extname(items[i]) === "." + fileExt) {
-            console.log(items[i]);
+fs.readdir(filePath, (err, items) => {
+    items.forEach((item) => {
+        if(path.extname(item) === '.' + fileExt) {
+            console.log(item);
         }
-    }
+    })
 });
  
