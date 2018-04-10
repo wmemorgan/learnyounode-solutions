237fdc8deb447e846f4c153716589197c99a75e2
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-10 12:18:05 +0000

    HTTP COLLECT (Exercise 8 of 13)  (Append Data Method) Changes to be committed: 	modified:   program.js

--- program.js
+++ program.js
@@ -1,9 +1,14 @@
-//Exercise 7: HTTP Client
+//Exercise 8: HTTP Collect
+//append data method
 
 const http = require('http');
 let url = process.argv[2];
-
+let completeData = '';
 http.get(url, (res) => {
     res.setEncoding('utf8');
-    res.on('data', (data) => { console.log(data) });
+    res.on('data', (data) => { completeData += data });
+    res.on('end', () => {
+        console.log(completeData.length);
+        console.log(completeData);
+    });
 }).on('error', (e) => { console.log("Got error: " + e.message) });
