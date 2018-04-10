4e993d06dd5ca4af61605f92e4211e8592fa4204
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-10 12:37:04 +0000

    HTTP COLLECT (Exercise 8 of 13)  (Third-party Package Method) Changes to be committed:   modified:   program.js

--- program.js
+++ program.js
@@ -1,14 +1,16 @@
 //Exercise 8: HTTP Collect
-//append data method
+//using third-party package to manage data
 
-const http = require('http');
+const http = require('http'),
+ BufferList = require('bl'), //third-party package
+ bl = new BufferList();
 let url = process.argv[2];
-let completeData = '';
+
 http.get(url, (res) => {
     res.setEncoding('utf8');
-    res.on('data', (data) => { completeData += data });
+    res.on('data', (data) => { bl.append(data) });
     res.on('end', () => {
-        console.log(completeData.length);
-        console.log(completeData);
+        console.log(bl.length);
+        console.log(bl.toString());
     });
 }).on('error', (e) => { console.log("Got error: " + e.message) });
