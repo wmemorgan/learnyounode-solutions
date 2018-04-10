414f0e972cce018ae32a607152af15d2082efeb5
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-10 11:28:37 +0000

    HTTP CLIENT (Exercise 7 of 13) (Production Release)
    Changes to be committed:
    	modified:   program.js

--- program.js
+++ program.js
@@ -2,29 +2,8 @@
 
 const http = require('http');
 let url = process.argv[2];
-// console.log("URL is:", url);
-// for (let i = 0; i < process.argv.length; i++) {
-//     console.log(i, process.argv[i]);
-// }
 
 http.get(url, (res) => {
-    const { statusCode } = res;
-    // const contentType = res.headers['content-type'];
-    // console.log("Got response: " + statusCode);
-    // res.setEncoding('utf8');
-    let rawData = '';
-    res.on('data', (data) => { 
-        // rawData += data;
-        console.log(String(data));
-        
-    });
-    // res.on('end', () => {
-    //     // const parsedData = JSON.parse(rawData);
-    //     // console.log(parsedData);
-    //     console.log(typeof rawData);
-    //     console.log(rawData);
-    // })
-    
-}).on('error', (e) => {
-    console.log("Got error: " + e.message);
-});
+    res.setEncoding('utf8');
+    res.on('data', (data) => { console.log(data) });
+}).on('error', (e) => { console.log("Got error: " + e.message) });
