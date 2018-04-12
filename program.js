4c8cfdf3d5abe60b453098de03499201f2fd1004
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-12 13:28:20 +0000

    HTTP JSON API SERVER (Exercise 13 of 13) [based on tutorial by Daniel Paul Grech Pereira]
     On branch ques13
     Changes to be committed:
    	modified:   program.js

--- program.js
+++ program.js
@@ -1,23 +1,57 @@
 //Exercise 13: JSON API Server
-//
+//based on tutorial by Daniel Paul Grech Pereira
 
 const http = require('http'),
- map = require('through2-map');
+ url = require('url');
+
+const getTimeStamp = (time) => {
+ return Date.parse(time);
+}
+
+const getUnixTimeStamp = (time) => {
+ return {
+  unixtime: getTimeStamp(time)
+ }
+}
+
+const getTimeObj = (time) => {
+ let date = new Date(getTimeStamp(time));
+ return {
+  hour:date.getHours(),
+  minute:date.getMinutes(),
+  second:date.getSeconds()
+ };
+}
 
 const server = http.createServer((req, res) => {
-    if (req.method === 'POST') {
-        req.pipe(map( (chunk) => {
-            return chunk.toString().toUpperCase();
-        })).pipe(res);
-    }
+ let urlObject = url.parse(req.url, true),
+ urlTime = urlObject.query.iso,
+ result;
+ 
+ if (urlObject.pathname === '/api/parsetime') {
+  result = getTimeObj(urlTime);
+ } else if (urlObject.pathname === '/api/unixtime') {
+  result = getUnixTimeStamp(urlTime);
+ }
+ 
+ if (result) {
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(result));
+ } else {
+  res.writeHead(404);
+  res.end();
+ }
+    
 }).listen(process.argv[2]);
 
 
 /*NOTES
+//Listen on port process.argv[2]
 
-ENDPOINT
+FIRST ENDPOINT
 //1.Receive GET request
 if (path === '/api/parsetime') 
+proceed to next step
 
 //2.Send data in JSON format
 parse ISO-format time into:
@@ -25,19 +59,16 @@ parse ISO-format time into:
 -minute
 -second
 
-//3.Listen on port process.argv[2]
 
 SECOND ENDPOINT
 
 //1.Receive GET request
 if (path === '/api/unixtime')
+proceed to next step
 
 //2.Send data in JSON format
 parse ISO-format time into:
 -milliseconds
-
-//3.Listen on port process.argv[2]
-
 
 */
 
