244a68cd1615d07299340dbadba1ef1fe5081a5a
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-12 00:28:27 +0000

    HTTP JSON API SERVER (Exercise 13 of 13) [Pseudo Code] On branch ques13  Changes to be committed: 	modified:   program.js

--- program.js
+++ program.js
@@ -1,5 +1,5 @@
-//Exercise 12: HTTP UPPERCASER
-//based on tutorial by Daniel Paul Grech Pereira
+//Exercise 13: JSON API Server
+//
 
 const http = require('http'),
  map = require('through2-map');
@@ -11,6 +11,36 @@ const server = http.createServer((req, res) => {
         })).pipe(res);
     }
 }).listen(process.argv[2]);
+
+
+/*NOTES
+
+ENDPOINT
+//1.Receive GET request
+if (path === '/api/parsetime') 
+
+//2.Send data in JSON format
+parse ISO-format time into:
+-hour
+-minute
+-second
+
+//3.Listen on port process.argv[2]
+
+SECOND ENDPOINT
+
+//1.Receive GET request
+if (path === '/api/unixtime')
+
+//2.Send data in JSON format
+parse ISO-format time into:
+-milliseconds
+
+//3.Listen on port process.argv[2]
+
+
+*/
+
 
 
 
